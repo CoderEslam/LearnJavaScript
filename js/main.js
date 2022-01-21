c0e97aef6879150d5c0dec7c9fca887a5a67242c
eslam <eslamfgazy600@gmail.com>
2022-01-21 16:19:22 +0200

    mypro

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//  console.log("hi");
+// var x = 5;
+// // console.log(x);
+// var l = ['k',5,8,'j'];
+// // console.log(l.shift());
+// for (i in l){
+//   // console.log(i);
+// }
+//
+//
+//
+// var date  = new Date();
+//
+//  print(date.getDay())
+//  print(date.getHours())
+//  print(date.getFullYear())
+//  print(date.getTime())
+//  print(date.getMinutes())
+//  print(date.getMonth())
+//  print(date.getMilliseconds())
+//  print(date.getDate())
+//
+//
+//  function print(p){
+//   console.log("show ",p)
+// }
+
+
+document.querySelector("#name").innerHTML = "Ghazyyyyyyyyyyyyyyy";
